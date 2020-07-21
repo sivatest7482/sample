03a6813ded01224c82fbb76c86467510983cfa43
sveerise <sveerise@LIN37001253.corp.capgemini.com>
2020-07-21 13:09:50 +0530

    test adding code

---
+++ src/test/java/com/sankartest/sample/test_selenium/TestingGithub.java
@@ -0,0 +1,11 @@
+package com.sankartest.sample.test_selenium;
+
+public abstract class TestingGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
