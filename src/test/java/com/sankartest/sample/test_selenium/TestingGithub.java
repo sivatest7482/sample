82dfac919738ea1ebe6f46f6de7e85891ab68111
sivatest7482 <68541569+sivatest7482@users.noreply.github.com>
2020-07-29 20:50:44 +0530

    Update TestingGithub.java
    changed class name

--- src/test/java/com/sankartest/sample/test_selenium/TestingGithub.java
+++ src/test/java/com/sankartest/sample/test_selenium/TestingGithub.java
@@ -2,7 +2,7 @@ package com.sankartest.sample.test_selenium;
 
 
 
-public abstract class TestingGithub {
+public class TestingGithub {
 
 	public static void main(Strings[] args) {
 		// TODO Auto-generated method stub
